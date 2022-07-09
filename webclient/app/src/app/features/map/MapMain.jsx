7a62501d3fb341581b96c29617dff2e65bd21b3b
Anett Hübner <anett_huebner@gmx.de>
2022-07-09 13:28:45 +0200

    add rest call

--- webclient/app/src/app/features/map/MapMain.jsx
+++ webclient/app/src/app/features/map/MapMain.jsx
@@ -1,21 +1,47 @@
+import React, {useState, useMemo, useEffect} from "react";
 import {Container, Typography} from "@mui/material";
-import React from "react";
 import {useTranslation} from "react-i18next";
-import {MapContainer, TileLayer, useMap, CircleMarker, Popup} from "react-leaflet";
+import {MapContainer, TileLayer, CircleMarker, Popup} from "react-leaflet";
 import "./MapMain.css";
 import "leaflet/dist/leaflet.css";
+import TrafficRecorderRest from "../../services/TrafficRecorderRest";
 
 function MapMain() {
-    const coords = [52.427183696557591, 10.776275400214885];
+    const trafficrecorderRest = useMemo(() => new TrafficRecorderRest(), []);
+    const [trafficRecorderAll, setTrafficRecorderAll] = useState();
+    const [center, setCenter] = useState([52.427183696557591, 10.776275400214885]);
+
+    useEffect(() => {
+        reload();
+    }, []);
+
+    function reload() {
+        trafficrecorderRest.findAll().then(response => {
+            setTrafficRecorderAll(response.data);
+            if (!!response.data && !!response.data[0].latitude && !!response.data[0].longitude) {
+                setCenter([response.data[0].latitude, response.data[0].longitude]);
+            }
+        });
+    }
 
     return (
         <>
-            <MapContainer center={coords} zoom={12} maxZoom={19} scrollWheelZoom={false} >
+            <MapContainer center={center} zoom={12} maxZoom={19} scrollWheelZoom={false} >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <CircleMarker key="test" center={coords} radius={9}>
+                {/*
+                {trafficRecorderAll.map(element => {
+                    return (
+                        <CircleMarker key={element.id} center={[element.latitude, element.longitude]} radius={9}>
+                            <Popup>
+                                <h2>{element.name}</h2> <br /> Latitude: {element.latitude} , Longitude: {element.longitude}
+                            </Popup>
+                        </CircleMarker>
+                    );
+                })} */}
+                <CircleMarker key="test" center={center} radius={9}>
                     <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
