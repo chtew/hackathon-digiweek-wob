e49efcc3ba51ea4346c01b8d4cf38282d0e8a010
Anett Hübner <anett_huebner@gmx.de>
2022-07-09 11:30:25 +0200

    circle marker

--- webclient/app/src/app/features/map/MapMain.jsx
+++ webclient/app/src/app/features/map/MapMain.jsx
@@ -1,27 +1,27 @@
 import {Container, Typography} from "@mui/material";
 import React from "react";
 import {useTranslation} from "react-i18next";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import {MapContainer, TileLayer, useMap, CircleMarker, Popup} from "react-leaflet";
 import "./MapMain.css";
-import 'leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
 
 function MapMain() {
     const coords = [52.427183696557591, 10.776275400214885];
 
     return (
         <>
-        <MapContainer center={coords} zoom={12} maxZoom={19} scrollWheelZoom={false} >
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={coords}>
-                <Popup>
+            <MapContainer center={coords} zoom={12} maxZoom={19} scrollWheelZoom={false} >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <CircleMarker key="test" center={coords} radius={9}>
+                    <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-        </MapContainer>
-            </>
+                    </Popup>
+                </CircleMarker>
+            </MapContainer>
+        </>
     );
 }
 
@@ -30,12 +30,12 @@ function CreateMap() {
 
     return (
         <Container>
-                <Typography variant={"h2"} gutterBottom>
-                    {t("ein titel")}
-                </Typography>
-                {t("der inhalt")}
+            <Typography variant={"h2"} gutterBottom>
+                {t("ein titel")}
+            </Typography>
+            {t("der inhalt")}
 
-                <MapMain />
+            <MapMain />
 
         </Container>
     );
