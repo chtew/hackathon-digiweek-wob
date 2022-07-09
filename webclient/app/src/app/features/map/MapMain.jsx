259ac77aaaf78239e0859a0074c1318f44d94d57
Cedric Rische <cedric.rische@cedsoft.de>
2022-07-09 20:11:49 +0200

    Added timerange

--- webclient/app/src/app/features/map/MapMain.jsx
+++ webclient/app/src/app/features/map/MapMain.jsx
@@ -1,5 +1,17 @@
 import React, {useEffect, useMemo, useState} from "react";
-import {AppBar, Button, CircularProgress, Dialog, IconButton, Slide, Toolbar, Typography} from "@mui/material";
+import {
+    AppBar,
+    Button,
+    CircularProgress,
+    Container,
+    Dialog,
+    IconButton,
+    Slide,
+    Stack,
+    TextField,
+    Toolbar,
+    Typography
+} from "@mui/material";
 import {useTranslation} from "react-i18next";
 import {CircleMarker, MapContainer, TileLayer} from "react-leaflet";
 import "./MapMain.css";
@@ -7,6 +19,7 @@ import "leaflet/dist/leaflet.css";
 import TrafficRecorderRest from "../../services/TrafficRecorderRest";
 import {Close} from "@mui/icons-material";
 import {TransitionProps} from '@mui/material/transitions';
+import moment from "moment";
 
 
 const Transition = React.forwardRef(function Transition(
@@ -19,19 +32,25 @@ const Transition = React.forwardRef(function Transition(
 });
 
 function MapMain() {
-    const trafficrecorderRest = useMemo(() => new TrafficRecorderRest(), []);
+    const trafficRecorderRest = useMemo(() => new TrafficRecorderRest(), []);
     const [trafficRecorderAll, setTrafficRecorderAll] = useState();
     const [center, setCenter] = useState([52.427183696557591, 10.776275400214885]);
     const [dialogEntity, setDialogEntity] = useState(null);
     const [loadedImage, setLoadedImage] = useState(false);
+    const [spanStart, setSpanStart] = useState("2020-05-01");
+    const [spanEnd, setSpanEnd] = useState("2020-06-01");
     const {t} = useTranslation();
 
     useEffect(() => {
         reload();
     }, []);
 
+    useEffect(() => {
+        setLoadedImage(false);
+    },[spanStart, spanEnd])
+
     function reload() {
-        trafficrecorderRest.findAll().then(response => {
+        trafficRecorderRest.findAll().then(response => {
             setTrafficRecorderAll(response.data);
             if (!!response.data && !!response.data[0].latitude && !!response.data[0].longitude) {
                 setCenter([response.data[0].latitude, response.data[0].longitude]);
@@ -52,7 +71,7 @@ function MapMain() {
 
     function renderLoadedImage() {
         let content = [<img key={"image"}
-                            src={"http://localhost:3001/render/d-solo/RY0Euae7z/miv-pro-rekorder?orgId=1&from=1588284000000&to=1590962399000&theme=light&panelId=3&width=1000&height=500&tz=Europe%2FBerlin&var-externalId=" + dialogEntity?.externalId}
+                            src={"http://localhost:3001/render/d-solo/RY0Euae7z/miv-pro-rekorder?orgId=1&from=" + moment(spanStart).unix()*1000 + "&to=" + moment(spanEnd).unix()*1000  + "&theme=light&panelId=3&width=1000&height=500&tz=Europe%2FBerlin&var-externalId=" + dialogEntity?.externalId}
                             onLoad={() => setLoadedImage(true)}/>];
 
         if (!loadedImage) {
@@ -65,7 +84,7 @@ function MapMain() {
         return (
             <Dialog
                 fullScreen
-                open={dialogEntity}
+                open={Boolean(dialogEntity)}
                 onClose={handleClose}
                 TransitionComponent={Transition}
             >
@@ -84,7 +103,39 @@ function MapMain() {
                         </Typography>
                     </Toolbar>
                 </AppBar>
-                {renderLoadedImage()}
+                <Container>
+                    <Stack direction={"row"} spacing={2}>
+                        <TextField
+                            id="start"
+                            label="Start"
+                            type="date"
+                            value={spanStart}
+                            sx={{width: 220}}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            onChange={(evnt) => {
+                                setSpanStart(evnt.target.value)
+                            }}
+                        />
+                        <TextField
+                            id="end"
+                            label="Ende"
+                            type="date"
+                            value={spanEnd}
+                            sx={{width: 220}}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            onChange={(evnt) => {
+                                setSpanEnd(evnt.target.value)
+                            }}
+
+                        />
+                    </Stack>
+
+                    {renderLoadedImage()}
+                </Container>
             </Dialog>
         )
     }
@@ -123,11 +174,6 @@ function MapMain() {
     );
 }
 
-function UploadCsv() {
-    return (
-        <Button href="#text-buttons">IMPORT CSV</Button>
-    );
-}
 
 function CreateMap() {
     const {t} = useTranslation();
@@ -137,10 +183,15 @@ function CreateMap() {
             <Typography variant={"h2"} gutterBottom>
                 {t("cityMap.title")}
             </Typography>
-            <UploadCsv/>
             <MapMain/>
         </>
     );
+
+    function uploadCsv() {
+        return (
+            <Button href="#text-buttons">IMPORT CSV</Button>
+        );
+    }
 }
 
 export default CreateMap;
