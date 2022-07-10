a4a59a2fe19b76cd92e6a15900f88204b600f646
Cedric Rische <cedric.rische@cedsoft.de>
2022-07-10 13:02:11 +0200

    Added heatmap and stuff

--- webclient/app/src/app/features/map/MapMain.jsx
+++ webclient/app/src/app/features/map/MapMain.jsx
@@ -1,19 +1,22 @@
 import React, {useEffect, useMemo, useState} from "react";
 import {
-    AppBar, Box,
+    AppBar,
+    Box,
     Button,
     CircularProgress,
     Container,
     Dialog,
     IconButton,
     Slide,
+    Slider,
     Stack,
     TextField,
     Toolbar,
     Typography
 } from "@mui/material";
 import {useTranslation} from "react-i18next";
-import {CircleMarker, MapContainer, TileLayer} from "react-leaflet";
+import {latLngBounds} from 'leaflet';
+import {CircleMarker, MapContainer, TileLayer, useMap} from "react-leaflet";
 import "./MapMain.css";
 import "leaflet/dist/leaflet.css";
 import TrafficRecorderRest from "../../services/TrafficRecorderRest";
@@ -36,14 +39,43 @@ const Transition = React.forwardRef(function Transition(
 });
 
 
+function MapCenterer(props) {
+    const map = useMap()
+
+    function setMarkerBounds(elements) {
+        let markerBounds = latLngBounds([]);
+        elements.forEach(element => {
+            markerBounds.extend([element.latitude, element.longitude])
+        });
+        return markerBounds;
+    }
+
+    useEffect(() => {
+        if (props.pointerData) {
+            map.fitBounds(setMarkerBounds(props.pointerData));
+        }
+
+    }, [props.pointerData])
+
+    return null
+}
+
 function MapMain() {
     const trafficRecorderRest = useMemo(() => new TrafficRecorderRest(), []);
     const [trafficRecorderAll, setTrafficRecorderAll] = useState();
-    const [center, setCenter] = useState([52.427183696557591, 10.776275400214885]);
+    const [center, setCenter] = useState({
+        lat: 52.427183696557591,
+        lng: 10.776275400214885,
+        zoom: 13
+    });
     const [dialogEntity, setDialogEntity] = useState(null);
     const [loadedImage, setLoadedImage] = useState(false);
     const [spanStart, setSpanStart] = useState("2020-05-01");
     const [spanEnd, setSpanEnd] = useState("2020-06-01");
+    const [steps, setSteps] = useState(null);
+    const [sliderValue, setSliderValue] = useState(null);
+    const [trafficRecorderMap, setTrafficRecorderMap] = useState(null);
+
     const {t} = useTranslation();
 
     useEffect(() => {
@@ -54,12 +86,46 @@ function MapMain() {
         setLoadedImage(false);
     }, [spanStart, spanEnd])
 
+
+    function calculateSteps(records) {
+        const steps = [];
+        records?.forEach(element => {
+            element.trafficRecord?.forEach(traffic => {
+                if (!steps.find(trafficFind => trafficFind.value === traffic.recordDate)) {
+                    steps.push({value: traffic.recordDate, label: moment(traffic.recordDate).toISOString()});
+                }
+            })
+        })
+        steps.sort((step1, step2) => {
+            if (step1.value < step2.value) {
+                return -1;
+            }
+            if (step1.value > step2.value) {
+                return 1;
+            }
+        });
+        setSliderValue(steps[0].value);
+        return steps
+    }
+
     function reload() {
         trafficRecorderRest.findAll().then(response => {
             setTrafficRecorderAll(response.data);
             if (!!response.data && !!response.data[0].latitude && !!response.data[0].longitude) {
                 setCenter([response.data[0].latitude, response.data[0].longitude]);
             }
+            let dataBlubb = JSON.parse(JSON.stringify(response.data))
+            dataBlubb.map(trafficRecord1 => {
+                let traMap = new Map();
+                trafficRecord1.trafficRecord.forEach((tr) => {
+                    traMap.set(tr.recordDate, tr.carCount);
+                })
+                trafficRecord1.trafficRecord = traMap;
+                return trafficRecord1
+            })
+
+            setTrafficRecorderMap(dataBlubb)
+            setSteps(calculateSteps(response.data));
         });
     }
 
@@ -75,7 +141,7 @@ function MapMain() {
     }
 
     function renderLoadedImage() {
-        if (!dialogEntity){
+        if (!dialogEntity) {
             return null;
         }
         let content = [<img key={"image"}
@@ -142,7 +208,6 @@ function MapMain() {
 
                         />
                     </Stack>
-
                     {renderLoadedImage()}
                 </Container>
             </Dialog>
@@ -151,30 +216,63 @@ function MapMain() {
 
     function calculatePoints() {
         const data = [];
-        trafficRecorderAll?.forEach(element => {
-            data.push([element?.latitude, element?.longitude, element?.trafficRecord[0]?.carCount])
+        trafficRecorderMap?.forEach(element => {
+            let foundTrafficRec = element.trafficRecord.get(sliderValue);
+            if (element.trafficRecord.has(sliderValue)) {
+                data.push([element?.latitude, element?.longitude, element.trafficRecord.get(sliderValue)])
+            }
         })
         return data;
     }
 
 
+    function valueText(value) {
+        return value;
+    }
+
+    function valueLabelFormat(value) {
+        return moment(value).format("DD.MM.YYYY");
+    }
+
+    function renderSlider() {
+        if (steps) {
+
+            return (
+                <Box pt={15}>
+                    <Slider
+                        defaultValue={steps[0].value}
+                        min={steps[0].value}
+                        getAriaValueText={valueText}
+                        valueLabelFormat={valueLabelFormat}
+                        max={steps[steps.length - 1].value}
+                        valueLabelDisplay="auto"
+                        value={sliderValue}
+                        step={null}
+                        marks={steps}
+                        onChange={(value) => {
+                            setSliderValue(value.target.value)
+                        }}
+                    />
+                </Box>
+            )
+        }
+
+        return <Typography variant={"body1"}>Loading</Typography>
+    }
+
     return (
         <>
-            <MapContainer center={center} zoom={12} maxZoom={19} scrollWheelZoom={true}>
-                <HeatmapLayer
-                    fitBoundsOnLoad
-                    fitBoundsOnUpdate
-                    points={calculatePoints()}
-                    fillOpacity={1}
-                    longitudeExtractor={m => m[1]}
-                    latitudeExtractor={m => m[0]}
-                    intensityExtractor={m => parseFloat(m[2])}/>
+            <MapContainer center={center} zoom={15} maxZoom={18} scrollWheelZoom={true}>
+                <MapCenterer pointerData={trafficRecorderAll}/>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 {
                     trafficRecorderAll?.map(element => {
+                        if (element.cityDirection === "inVW" || element.cityDirection === "outVW") {
+                            return;
+                        }
                         return (
                             <>
                                 <CircleMarker key={"data" + element.id} center={[element.latitude, element.longitude]}
@@ -185,13 +283,30 @@ function MapMain() {
                                     },
                                 }}>
                                 </CircleMarker>
+
                             </>
                         );
                     })
                 }
-            </MapContainer>
-            {renderDialog()}
 
+                <HeatmapLayer
+                    maxZoom={18}
+                    zoom={15}
+                    fitBoundsOnLoad
+                    fitBoundsOnUpdate
+                    fillOpacity={1}
+                    points={calculatePoints()}
+                    longitudeExtractor={m => m[1]}
+                    latitudeExtractor={m => m[0]}
+                    intensityExtractor={m => parseFloat(m[2])}
+                    radius={40}
+                    blur={50}
+                    local
+                />
+            </MapContainer>
+            {renderSlider()}
+
+            {renderDialog()}
         </>
     );
 }
@@ -266,6 +381,7 @@ function CreateMap() {
                 </Box>
             </Stack>
             <MapMain/>
+
         </>
     );
 
