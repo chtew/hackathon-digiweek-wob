10e70e9de2a0217f5b37cd83578e619c5db24cf8
Anett Hübner <anett_huebner@gmx.de>
2022-08-13 15:25:33 +0200

    added single chart for grafana

--- application/src/main/java/de/digiweek/application/config/CorsConfig.java
+++ application/src/main/java/de/digiweek/application/config/CorsConfig.java
@@ -6,18 +6,18 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Profile("!dev")
+//@Profile("!dev")
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Value("${allowedorigins}")
-    private String[] allowedorigins;
+    private String allowedorigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins(allowedorigins)
+                .allowedOrigins(allowedorigins.split(","))
                 .allowedHeaders("*")
                 .allowCredentials(false);
     }
